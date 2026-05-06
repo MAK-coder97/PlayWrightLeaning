07b11117f045e7584918259c79a69be8778de4e4
MAK <mohammed.tandem@gmail.com>
2026-05-06 21:20:55 +0530

    Update Chapter_01_basics/01_Basics.js

--- Chapter_01_basics/01_Basics.js
+++ Chapter_01_basics/01_Basics.js
@@ -5,3 +5,4 @@ console.log("Hello, " + name + "!");
 console.log(process.platform);
 console.log(process.arch);
 console.log("node version: " + process.version);
+console.log("Current directory: " + process.cwd());
