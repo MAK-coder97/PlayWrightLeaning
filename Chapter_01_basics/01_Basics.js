6dd8065c6fd1f6a3b54e3d59d47ab91bc7ccbf3a
MAK <mohammed.tandem@gmail.com>
2026-05-06 21:09:12 +0530

    Initial commit

---
+++ Chapter_01_basics/01_Basics.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+let name = "Alice";
+console.log("Hello, " + name + "!");
+
+console.log(process.platform);
+console.log(process.arch);
+console.log("node version: " + process.version);
