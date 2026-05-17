d8ed4b3fef5fd2f85f50f1d1cf03a5ccb4166165
MAK <mohammed.tandem@gmail.com>
2026-05-17 13:31:50 +0530

    added few more lines

--- Chapter_01_basics/Chapter04_JS_concepts/04_11_var_explained.js
+++ Chapter_01_basics/Chapter04_JS_concepts/04_11_var_explained.js
@@ -6,4 +6,20 @@ function pritngtHello() {
     console.log("Value of a inside function: " + a); // Output: 10
 }
 pritngtHello();
+//
+//console.log("Value of x outside function: " + x); // Output: 10
+//console.log(typeof x); // Output: 10
+//const x = 20;// 
 
+console.log("Value of y outside function: " + y); // Output: 20
+var y = 30;
+
+sayHello();
+const sayHello = function () {
+    console.log("Hello, World!");
+}
+
+console.log(testName);
+let testName = "Login Test";
+
+let count = count + 1; // ReferenceError: Cannot access 'count' before initialization
