8bf442f5fce56dcdc884e00f29bee2ffcfc76c0c
MAK <mohammed.tandem@gmail.com>
2026-05-17 11:08:51 +0530

    Update README with full file listing and topics covered table

--- Chapter_01_basics/Chapter04_JS_concepts/04_11_var_explained.js
+++ Chapter_01_basics/Chapter04_JS_concepts/04_11_var_explained.js
@@ -1,1 +1,9 @@
-git 
+var a = 10;
+console.log(a); // Output: 10
+
+function pritngtHello() {
+    console.log("Hello, World!");
+    console.log("Value of a inside function: " + a); // Output: 10
+}
+pritngtHello();
+
