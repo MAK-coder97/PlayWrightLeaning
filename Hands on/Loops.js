5dacfee942f8cfd6049f63802436e100040f1e5e
MAK <mohammed.tandem@gmail.com>
2026-06-06 00:08:03 +0530

    Add Loops.js to Hands on folder
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ Hands on/Loops.js
@@ -0,0 +1,39 @@
+/* const obj = { name: "Aasim", age: 22 };
+
+for (const key in obj) {
+    console.log(key, obj[key]);
+}
+ */
+// For , For of, For in , ForEach
+
+//for ( 1,C,U)
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+
+}
+
+//FOR of 
+
+// While Loop 
+
+/* While loop also have I.C.U */
+
+let attempt = 0;
+
+while (attempt < 3) {
+    console.log(attempt);
+    attempt++;
+
+}
+
+for (let i = 0; i < 3; i++) {
+    if (i === 1) continue;
+    console.log(i);
+}
+
+let count = 10;
+while (count < 5) {
+    console.log(count);
+    count++;
+}
