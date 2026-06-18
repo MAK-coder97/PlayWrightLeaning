54dc7a90e19a1e391d8646dc0aa621ce49545062
MAK <mohammed.tandem@gmail.com>
2026-06-18 07:47:07 +0530

    feat: add Playwright hands-on test files
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ Hands on/Playwright_Handson/tta-cart.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    await page.goto('https://app.thetestingacademy.com/playwright/ttacart/');
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="password"]').click();
+    await page.locator('[data-test="username"]').click();
+    await page.locator('[data-test="username"]').fill('Mohammed');
+    await page.locator('[data-test="username"]').press('Tab');
+    await page.locator('[data-test="password"]').fill('Test@123');
+    await page.locator('[data-test="login-button"]').click();
+    await page.locator('[data-test="error"]').click();
+
+});
